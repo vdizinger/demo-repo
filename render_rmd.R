3b44810e83db7e55b0ba6e55478eec28efbdd7ef
Johannes Breuer <johannes.breuer@gesis.org>
2021-11-18 14:08:10 +0100

    create second report

--- render_rmd.R
+++ render_rmd.R
@@ -1,1 +1,3 @@
 rmarkdown::render('./src/Report1_Obeying_Curfew.Rmd', output_file = '../output/Report1_Obeying_Curfew.html')
+
+rmarkdown::render('./src/Report2_News_sources.Rmd', output_file = '../output/Report2_News_sources.pdf')
